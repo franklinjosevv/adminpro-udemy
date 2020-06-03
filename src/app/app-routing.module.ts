d87e034789cba535ca6720ef4a6b29efa3f1c122
franklin vasquez <franklinjosevv@ufps.edu.co>
2020-06-03 17:41:20 -0500

    modificaciones en los modulos de pages y routes de los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
+// import { ProgressComponent } from './pages/progress/progress.component';
+// import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+// import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
 const routes: Routes = [
-  {path: '', 
+  /*{path: '',
   component: PagesComponent,
   children: [
     { path: 'dashboard', component: DashboardComponent},
@@ -18,11 +18,9 @@ const routes: Routes = [
     { path: 'graficas1', component: Graficas1Component},
     { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
   ]
-},
-  
+},*/
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  
   // { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: '**', component: NopagefoundComponent}
 ];
